93d8edfba3a2c67dca8d85729bef11e76a764723
Mateo <you@example.com>
2023-07-28 15:17:41 -0500

    Ammended and finished 7/28/23 class

--- M1files/repl.py
+++ M1files/repl.py
@@ -6,7 +6,7 @@ from M1files.tokens import(
 
 EOF_TOKEN:Token=Token(TokenType.EOF, '')
 def star_repel()->None:
-    while(source:=input('>> '))!='salir()':
+    while(source:=input('>> '))!='close()':
         lexer:Lexer=Lexer(source)
 
         while(token:=lexer.next_token())!=EOF_TOKEN:

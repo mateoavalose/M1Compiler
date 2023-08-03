dc264eab8321b861260f249cf74667280025e254
Mateo <mateo.avalos3942@gmail.com>
2023-08-03 10:34:16 -0500

    Finished Lexer from 7/28/23 class

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from M1files.repl import star_repel
 
-print("Bienvenido al compilador M1")
 def main() -> None:
+    print("Bienvenido al Lenguaje de Programación M1")
     star_repel()
 if __name__ == "__main__":
     main()

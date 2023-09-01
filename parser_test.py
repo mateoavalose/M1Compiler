f5f48d23661db6ed4ca2ab2eedf759e95b42f30f
Mateo <mateo.avalos3942@gmail.com>
2023-08-31 23:39:36 -0500

    Extended the 'parser_test.py' test and modified the '.gitignore' file

--- parser_test.py
+++ parser_test.py
@@ -1,9 +1,13 @@
 from unittest import TestCase
+from typing import (
+    cast,
+    List,
+)
  
 from M1files.ast import(
     Expression,
     Identifier,
-    LesStatement,
+    LetStatement,
     Program,
     Statement
 )
@@ -35,4 +39,25 @@ class ParserTest(TestCase):
         self.assertEqual(len(program.statements), 3)
         for statement in program.statements:
             self.assertEqual(statement.token_literal(), 'variable')
-            self.assertIsInstance(statement, LesStatement)
+            self.assertIsInstance(statement, LetStatement)
+
+    def test_names_in_let_statements(self) -> None:
+        source: str = '''
+            variable x = 5;
+            variable y = 10;
+            variable foo = 20;
+        '''
+        lexer: Lexer = Lexer(source)
+        parser: Parser = Parser(lexer)
+
+        program: Program = parser.parse_program()
+
+        names: List[str] = []
+        for statement in program.statements:
+            statement = cast(LetStatement, statement)
+            assert statement.name is not None
+            names.append(statement.name.value)
+
+        expected_names: List[str] = ['x', 'y', 'foo']
+
+        self.assertEquals(names, expected_names)

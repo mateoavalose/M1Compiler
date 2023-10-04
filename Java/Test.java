923b573da71ed3ab9ec1c3d1e99f9c2234ba4e86
Mateo <mateo.avalos3942@gmail.com>
2023-10-03 19:31:15 -0500

    Deleted 'test.java' file

--- Java/Test.java
+++
@@ -1,6 +0,0 @@
-public class Test {
-    public static void main(String[] args){
-        String character = "5";
-        System.out.println("Character" + character + "is letter? " + Character.isLetter(character.charAt(0)));
-    }
-}
